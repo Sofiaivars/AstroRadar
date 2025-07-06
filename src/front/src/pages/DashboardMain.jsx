8a7d6120abdcd2a50c000671ef1d0a66755b5a45
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-07-06 19:18:17 +0200

    cambiado loader de carga de dashboard

--- src/front/src/pages/DashboardMain.jsx
+++ src/front/src/pages/DashboardMain.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { getUserInfo } from "../servicios/login-service";
 import { useNavigate } from "react-router";
-import { Helix } from "ldrs/react";
-import "ldrs/react/Helix.css";
 import EventoDestacado from "../components/dashboard/EventoDestacado";
 import Map from "../components/dashboard/Map";
 import RankingMain from "../components/dashboard/ranking-component/RankingMain";
@@ -17,6 +15,8 @@ import InfoTopComponent from "../components/dashboard/InfoTopComponent/InfoTopCo
 import { getUserLocation } from "../servicios/geolocation-service.js";
 import SideBar from "../components/sidebar/SideBar.jsx";
 import { getJSONCoords } from "../servicios/cosmo-service.js";
+import { Orbit } from "lucide-react";
+import PageLoader from "../components/loaders/PageLoader.jsx";
 
 function DashboardMain() {
   const [userData, getUserData] = useState({});
@@ -73,9 +73,14 @@ const fetchAI = async (lat, lon) => {
 
   if (Object.keys(userData).length === 0) {
     return (
-      <div className="flex flex-col gap-3 bg-gray-900 text-white rounded-3xl p-3">
-        <Helix size="60" speed="1.3" color="#ffffff" />
-        <button onClick={() => navigate("/")}>🏠</button>
+      <div className="flex flex-col items-center justify-center gap-3 p-3">
+        <div className="flex justify-center items-center">
+          <PageLoader />
+        </div>
+        
+        <button className="text-purple-300 hover:text-cyan-300 transition-colors duration-500 opacity-35 cursor-pointer z-10" onClick={() => navigate("/")}>
+          <Orbit/>
+        </button>
       </div>
     );
   }
