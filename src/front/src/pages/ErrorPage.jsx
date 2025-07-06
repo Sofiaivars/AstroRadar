cfb638c5018765d3d4a84ec71f1afce4efe063e5
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-07-06 20:23:19 +0200

    ErrorPage

--- src/front/src/pages/ErrorPage.jsx
+++ src/front/src/pages/ErrorPage.jsx
@@ -1,8 +1,26 @@
+import './ErrorPage.css'
+import hansTriangle from './assets/giferrorpage.gif'
+import { TriangleAlert  } from "lucide-react";
+import { useNavigate } from "react-router"
+
 function ErrorPage() {
+  const navigate = useNavigate()
 
   return(
-    <div>
-      Error 404
+    <div className='flex justify-center items-center'>
+      <img src={hansTriangle} alt="" />
+      <div className='flex flex-col'>
+        <h1 className='text-4xl'>Error 404</h1>
+        <div className='flex items-center gap-5 text-2xl'>
+          <p>Te perdiste en el espacio</p>
+          <TriangleAlert />
+        </div>
+        <button class="ui-btn rounded-2xl" onClick={() => navigate("/dashboard")}>
+          <span>
+            Inicio
+          </span>
+        </button>
+      </div>
     </div>
   )
 }
