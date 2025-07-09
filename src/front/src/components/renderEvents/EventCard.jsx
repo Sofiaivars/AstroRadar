33b7e82062de3b31973848b5bc0cea768e4c3a60
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-07-09 21:35:51 +0200

    CountdownComponent

--- src/front/src/components/renderEvents/EventCard.jsx
+++ src/front/src/components/renderEvents/EventCard.jsx
@@ -1,4 +1,5 @@
-import { CalendarDays, CalendarClock, CalendarOff, Telescope, Moon } from "lucide-react";
+import { CalendarDays, CalendarClock, CalendarOff, Telescope, Moon, Hourglass } from "lucide-react";
+import CountdownComponent from "./CountdownComponent";
 
 function EventCard({eventImg, eventName, eventCategory, eventStart, eventEnd, eventVisibility, eventMoon}){
 
@@ -16,16 +17,16 @@ function EventCard({eventImg, eventName, eventCategory, eventStart, eventEnd, ev
   }
 
   return(
-    <div className="flex rounded-2xl w-full items-center gap-3 bg-purple-300 borde-con-degradado">
+    <div className="flex rounded-2xl w-full items-center gap-3 text-sm bg-purple-300 borde-con-degradado">
       <div className="flex w-50 max-h-33 rounded-l-2xl overflow-hidden">
         <img src={eventImg} alt={eventCategory} className="w-full h-full object-cover" />
       </div>
       <div className="flex flex-col w-3/10 gap-3">
-        <div className="flex w-full gap-1">
+        <div className="flex items-center w-full gap-1">
           <CalendarDays/>
           <p>{eventName}</p>
         </div>
-        <div className="flex w-full gap-1">
+        <div className="flex items-center w-full gap-1">
           <CalendarClock />
           <p>
             {new Date(eventStart).toLocaleDateString('en-GB', {
@@ -35,7 +36,7 @@ function EventCard({eventImg, eventName, eventCategory, eventStart, eventEnd, ev
             })}
           </p>
         </div>
-        <div className="flex w-full gap-1">
+        <div className="flex items-center w-full gap-1">
           <CalendarOff />
           <p>
             {new Date(eventEnd).toLocaleDateString('en-GB', {
@@ -47,14 +48,19 @@ function EventCard({eventImg, eventName, eventCategory, eventStart, eventEnd, ev
         </div>
       </div>
       <div className="flex flex-col gap-3 w-3/10 me-3">
-        <div className="flex w-full gap-1">
+        <div className="flex items-center w-full gap-1">
           <Telescope />
           <p>{eventVisibility}</p>
         </div>
-        <div className="flex w-full gap-1">
+        <div className="flex items-center w-full gap-1">
           <Moon />
           <p>{`Luna ${eventMoon}`}</p>
         </div>
+        <div className="flex items-center w-full gap-1">
+          <Hourglass />
+          <CountdownComponent eventStart={eventStart}/>
+        </div>
+        
       </div>
       <div className="flex justify-end items-end w-2/10 me-3">
         <button className="bg-purple-700 hover:bg-purple-300 p-2 rounded-2xl borde-con-degradado" onClick={handleClick}>
