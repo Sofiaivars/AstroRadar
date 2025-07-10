7bf6543d3fea1d474150a7995c4281f3a00963d9
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-07-10 13:24:57 +0200

    pequeños cambios

--- src/front/src/components/dashboard/EventoDestacado.jsx
+++ src/front/src/components/dashboard/EventoDestacado.jsx
@@ -1,6 +1,7 @@
 const EventoDestacado = () => {
 
-    return (<>
+
+  return (<>
     <div className="p-[3px] rounded-xl w-full h-[140px]">
       <div className="flex w-full h-full bg-[var(--components-background)] rounded-xl overflow-hidden text-[var(--astroradar-white)] borde-con-degradado">
         
@@ -29,63 +30,55 @@ const EventoDestacado = () => {
           <p className="text-sm font-bold text-[var(--astroradar-white)] whitespace-nowrap">
             en 48hs 37min
           </p>
-          <button
-              
-              className="
-    group
-    rounded-[12px]
-    p-[1.5px]
-    text-white
-    text-sm
-    h-10
-    w-40
-    font-medium
-    transition
-    duration-300
-    flex
-    items-center
-    justify-center
-    hover:shadow-2xl
-    hover:shadow-purple-600/30
-  "
+          <button className="
+            group
+            rounded-[12px]
+            p-[1.5px]
+            text-white
+            text-sm
+            h-10
+            w-40
+            font-medium
+            transition
+            duration-300
+            flex
+            items-center
+            justify-center
+            hover:shadow-2xl
+            hover:shadow-purple-600/30"
+            style={{
+              backgroundImage:
+                "linear-gradient(var(--components-background), var(--components-background)), " +
+                "linear-gradient(to right, #a855f7, #d946ef, #22d3ee)", //bordeeeeeeeee
+              backgroundOrigin: "border-box",
+              backgroundClip: "padding-box, border-box",
+              border: "2px solid transparent",
+            }}
+          >
+            <div className="
+              rounded-[12px]
+              w-full
+              h-full
+              flex
+              items-center
+              justify-center
+              transition
+              duration-300
+              ease-in-out
+              group-hover:bg-gradient-to-br
+              group-hover:from-gray-700
+              group-hover:to-gray-900"
               style={{
-                backgroundImage:
-                  "linear-gradient(var(--components-background), var(--components-background)), " +
-                  "linear-gradient(to right, #a855f7, #d946ef, #22d3ee)", //bordeeeeeeeee
-                backgroundOrigin: "border-box",
-                backgroundClip: "padding-box, border-box",
-                border: "2px solid transparent",
+                backgroundColor: "var(--components-background)",
               }}
             >
-              <div
-                className="
-      rounded-[12px]
-      w-full
-      h-full
-      flex
-      items-center
-      justify-center
-      transition
-      duration-300
-      ease-in-out
-      group-hover:bg-gradient-to-br
-      group-hover:from-gray-700
-      group-hover:to-gray-900
-    "
-                style={{
-                  backgroundColor: "var(--components-background)",
-                }}
-              >
-                Comenzar misión
-              </div>
-            </button>
+              Comenzar misión
+            </div>
+          </button>
         </div>
       </div>
     </div>
-
-
-    </>)
-
+  </>)
 }
 
 export default EventoDestacado
