bd1ec0ea61d558111e1f8d5f8f8688768ba89441
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-07-15 16:21:46 +0000

    cuadro explicativo en el dashboard

--- src/front/src/components/dashboard/MapDashboard.jsx
+++ src/front/src/components/dashboard/MapDashboard.jsx
@@ -29,8 +29,8 @@ const MapDashboard = ({ locations, userPosition, onSelectBase }) => {
           <h3 className="text-base font-semibold mb-1">Bases Estelares</h3>
           <p className="text-sm mt-5">
             {store.selectedBase
-              ? `Base seleccionada: ${store.selectedBase.name}`
-              : 'SELECCIONA TU BASE ESTELAR'}
+              ? `Base añadida: ${store.selectedBase.name}`
+              : 'AÑADE TUS BASES ESTELARES, PODRÁS ELEGIRLAS AL COMENZAR UNA MISIÓN'}
           </p>
         </div>
       </div>
