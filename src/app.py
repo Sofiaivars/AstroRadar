66d2987a203e1f2dcb651e12f730de4873a8083b
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-07-23 15:18:06 +0200

    front/dist

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ import requests
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../dist/')
+    os.path.realpath(__file__)), './front/dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
