7a1363823e3f63fd693dcc63a884f6fd07698db3
Sofiaivars <sofiaivars1@gmail.com>
2025-07-23 16:54:41 +0200

    arreglo de z-index afectado por particles

--- src/front/src/components/AstroRadarFooter.jsx
+++ src/front/src/components/AstroRadarFooter.jsx
@@ -1,15 +1,20 @@
-import { useNavigate } from "react-router"
+import { useNavigate } from "react-router";
 
 function AstroRadarFooter() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  return(
-    <div className="flex text-sm w-full items-center justify-center gap-5 absolute bottom-0">
+  return (
+    <div className="flex text-sm w-full items-center justify-center gap-5 absolute bottom-0 z-10">
       <p className="opacity-40">&copy; 2025 AstroRadar Team</p>
       <p className="opacity-40">|</p>
-      <p className="hover:text-purple-50 opacity-40 hover:opacity-100 cursor-pointer" onClick={() => navigate("/about-us")}>About Us</p>
+      <p
+        className="hover:text-purple-50 opacity-40 hover:opacity-100 cursor-pointer"
+        onClick={() => navigate("/about-us")}
+      >
+        About Us
+      </p>
     </div>
-  )
+  );
 }
 
-export default AstroRadarFooter
+export default AstroRadarFooter;
