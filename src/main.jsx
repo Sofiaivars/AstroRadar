9d65bb35324955efe702f55a90e83650f1e7f516
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-12-05 13:20:44 +0100

    Redux store provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import router from '../routes.jsx'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <Provider store={store}>
       <RouterProvider router={router}/>
-    
+    </Provider>
   </StrictMode>,
 )
