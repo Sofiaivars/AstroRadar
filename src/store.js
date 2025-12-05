48ad0e0e828bf9bae88d118ee061e3c07da541b8
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-12-05 11:38:50 +0100

    selectedBase slice

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userLocationReducer from './features/userLocation/userLocationSlice';
 import suggestedCoordsReducer from './features/suggestedCoords/suggestedCoordsSlice';
+import selectedBaseReducer from './features/selectedBase/selectedBaseSlice';
 
 const options = {
   reducer: {
     userLocation: userLocationReducer,
     suggestedCoords: suggestedCoordsReducer,
+    selectedBase: selectedBaseReducer,
 
   }
 }
