8a78aa331b90eadc72193c0ecfc3f2b2802e5c17
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-12-05 13:17:03 +0100

    fetchEventsList

--- src/features/eventList/eventListSlice.js
+++ src/features/eventList/eventListSlice.js
@@ -1,17 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getEventsFromAPI } from "../../servicios/events-missions-service";
+
+const initialState = {
+  eventsList: [],
+  status: 'idle'
+}
+
+export const fetchEventList = createAsyncThunk(
+  'eventList/fetchEventList',
+  async (_, thunkAPI) => {
+    const response = await getEventsFromAPI();
+    return response;
+  }
+);
 
 const options = {
   name: 'eventList',
-  initialState: [],
+  initialState,
   reducers: {
     clearEventList: () => [],
-    setEventList: (state, action) => {
-      state.push(action.payload);
-    }
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchEventList.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchEventList.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.eventsList = [...action.payload];
+      })
+      .addCase(fetchEventList.rejected, (state) => {
+        state.status = 'rejected';
+      });
   }
 }
 
 const eventListSlice = createSlice(options);
 
-export const { clearEventList, setEventList } = eventListSlice.actions;
+export const { clearEventList } = eventListSlice.actions;
 export default eventListSlice.reducer;
